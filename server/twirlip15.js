a19924d065729c60377b980baf9d4b5adf19778d
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2020-06-10 08:36:18 -0400

    Use serve-index

--- server/twirlip15.js
+++ server/twirlip15.js
@@ -1,15 +1,17 @@
 console.log("Twirlip15")
 
-import path from "path"
+// import path from "path"
 import express from "express"
+import serveIndex from "serve-index"
 
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
 
 const host = "127.0.0.1"
 const port = 8080
 
 const app = express();
 app.use(express.static(process.cwd()))
+app.use(serveIndex(".", {'icons': true}))
 
 /*
 app.get("/", function(request, response) {
