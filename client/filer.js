3b7fd5e19a8551664ef3084454a35644ca94ec72
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2020-06-20 12:29:06 -0400

    filer: display file contents

--- client/filer.js
+++ client/filer.js
@@ -4,6 +4,8 @@ import "./vendor/mithril.js"
 let directoryPath = "/"
 let directoryFiles = null
 let errorMessage = ""
+let chosenFileName = ""
+let chosenFileContents = ""
 
 async function loadDirectory(newPath) {
     if (newPath.endsWith("../")) {
@@ -17,6 +19,8 @@ async function loadDirectory(newPath) {
     directoryPath = newPath
     directoryFiles = null
     errorMessage = ""
+    chosenFileName = ""
+    chosenFileContents = null
     const response = await fetch("/twirlip15-api", {
         method: "POST",
         headers: {
@@ -35,7 +39,32 @@ async function loadDirectory(newPath) {
         }   
     } else {
         console.log("HTTP-Error: " + response.status, response)
-        errorMessage = "API request failed: " + response.status
+        errorMessage = "API request failed for file-directory: " + response.status
+    }
+    m.redraw()
+}
+
+async function loadFileContents(newFileName) {
+    chosenFileName = newFileName
+    chosenFileContents = null
+    const response = await fetch("/twirlip15-api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({request: "file-contents", fileName: chosenFileName})
+    })
+    if (response.ok) {
+        const json = await response.json()
+        console.log("response", response)
+        if (json.ok) {
+            chosenFileContents = json.contents
+        } else {
+            errorMessage = json.errorMessage
+        }   
+    } else {
+        console.log("HTTP-Error: " + response.status, response)
+        errorMessage = "API request failed for file contents: " + response.status
     }
     m.redraw()
 }
@@ -43,9 +72,8 @@ async function loadDirectory(newPath) {
 function fileEntryView(fileInfo) {
     return fileInfo.isDirectory
         ? m("div", m("span", {onclick: () => loadDirectory(directoryPath + fileInfo.name + "/")}, fileInfo.name + " 📂"))
-        : m("div", fileInfo.name)
+        : m("div", m("span", {onclick: () => loadFileContents(directoryPath + fileInfo.name)}, fileInfo.name))
 }
-
 
 const Filer = {
     view: () => {
@@ -54,6 +82,9 @@ const Filer = {
             directoryFiles
                 ? m("div", directoryFiles.map(fileInfo => fileEntryView(fileInfo)))
                 : "Loading file data...",
+            chosenFileName && m("div.ml2.mt2", "Chosen file: " , chosenFileName),
+            (chosenFileName && (chosenFileContents === null)) && m("div", "Loading file contents..."),
+            (chosenFileContents !== null) && m("pre.ml2", {style: "white-space: pre-wrap;"}, chosenFileContents),
             errorMessage && m("div.red", errorMessage) 
         )
     }
