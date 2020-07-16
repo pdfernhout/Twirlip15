855aa59448434c391c4f46fdadea5f69a10fb53f
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2020-07-16 10:55:42 -0400

    mail: separate emails

--- client/mail.js
+++ client/mail.js
@@ -4,8 +4,10 @@ import { twirlip15ApiCall } from "./twirlip15-support.js"
 
 let errorMessage = ""
 let chosenFileName = ""
-let chosenFileContents = null
+let mboxContents = null
 let chosenFileLoaded = false
+
+let emails = []
 
 function showError(error) {
     errorMessage = error
@@ -13,29 +15,42 @@ function showError(error) {
 
 async function loadFileContents(newFileName) {
     chosenFileName = newFileName
-    chosenFileContents = null
+    mboxContents = null
     chosenFileLoaded = false
     const apiResult = await twirlip15ApiCall({request: "file-contents", fileName: chosenFileName}, showError)
     if (apiResult) {
-        chosenFileContents = apiResult.contents
+        mboxContents = apiResult.contents
         chosenFileLoaded = true
     } else {
-        chosenFileContents = ""
+        mboxContents = ""
     }
+    splitEmails()
+}
+
+function splitEmails() {
+    const emailsRaw  = mboxContents.split("From ")
+    emailsRaw.splice(0, 1)
+    emails = emailsRaw
 }
 
 function viewFileContents() {
-    return m("pre", chosenFileContents)
+    return m("div", emails.map(email => {
+        const lines = email.split("\n")
+        return m("div", 
+            m("pre", "From " + lines.slice(0, 4).join("\n")), 
+            m("hr")
+        )
+    }))
 }
 
 const ViewMail = {
     view: () => {
-        return m("div.ma2.measure-wide",
+        return m("div.ma2",
             errorMessage && m("div.red", m("span", {onclick: () => errorMessage =""}, "X "), errorMessage),
             !chosenFileName && m("div",
                 "file not specified in URL querystring"
             ),
-            chosenFileName && !chosenFileLoaded && chosenFileContents === null && m("div",
+            chosenFileName && !chosenFileLoaded && mboxContents === null && m("div",
                 "Loading..."
             ),
             chosenFileName && chosenFileLoaded && m("div",
