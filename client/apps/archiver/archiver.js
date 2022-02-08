79368e002e41fa5d6a710e413d91f7075aa46047
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2022-02-08 08:22:21 -0500

    archiver: wip on JSONStore with hashes circa 2021-05-26

--- client/apps/archiver/archiver.js
+++ client/apps/archiver/archiver.js
@@ -3,6 +3,11 @@ import "../../vendor/mithril.js"
 import { Twirlip15ServerAPI } from "../../common/twirlip15-api.js"
 import { interceptSaveKey } from "../../common/menu.js"
 
+import canonicalize from "../../vendor/canonicalize.js"
+
+/* global sha256 */
+import "../../vendor/sha256.js"
+
 let errorMessage = ""
 let chosenFileName = ""
 let contentsSaved = ""
@@ -11,12 +16,56 @@ let chosenFileLoaded = false
 let editing = false
 let editedContents = ""
 let fileSaveInProgress = false
+let sha256Saved = ""
 
 function showError(error) {
     errorMessage = error
 }
 
 const TwirlipServer = new Twirlip15ServerAPI(showError)
+
+// Code goes here for saving and loading from SHA256 hashes for content
+// Somehow need to associate a mimetype and/or extension with content,
+// or otherwise need to decide on a single format like JSON or a Unicode string
+
+// TODO: fix hardcoded local path
+const basePath = "/home/pdfernhout/JSONStore"
+
+function pathForSHA256(sha256String) {
+    const shaPath = [
+        sha256String.substring(0, 2), 
+        sha256String.substring(2, 4), 
+        sha256String.substring(4, 6)
+    ].join("/")
+    const path = basePath + "/" + shaPath
+    return path
+}
+
+// content is a JavaScript Object that can be converted to canonical JSON
+async function writeToJSONStore(object) {
+    const canonicalJSONString = canonicalize(object)
+    const sha256String = sha256(canonicalJSONString)
+    const path = pathForSHA256(sha256String)
+    // Maybe add optional path creation to fileSave on server
+    const apiResult1 = TwirlipServer.fileNewDirectory(path)
+    if (!apiResult1) throw new Error("Failed to create directory for JSON")
+    const fileName = path + "/" + sha256String + ".json"
+    const apiResult2 = await TwirlipServer.fileSave(fileName, canonicalJSONString)
+    if (!apiResult2) throw new Error("Failed to save JSON")
+    return sha256String
+}
+
+// returns JavaScript Object parsed from JSON
+async function readFromJSONStore(sha256String) {
+    const fileName = pathForSHA256(sha256String) + "/" + sha256String + ".json"
+    const apiResult = await TwirlipServer.fileContents(fileName)
+    if (!apiResult) throw new Error("Failed to read JSON")
+    return JSON.parse(apiResult.contents)
+}
+
+// Code goes here for named (as JSON SHA256) append-only file of SHA256 hashes
+
+/////////////////////////////////////
 
 async function loadFileContents(newFileName) {
     chosenFileName = newFileName
@@ -31,6 +80,7 @@ async function loadFileContents(newFileName) {
     }
 }
 
+/*
 async function saveFile(fileName, contents, successCallback) {
     if (fileSaveInProgress) return
     fileSaveInProgress = true
@@ -40,12 +90,18 @@ async function saveFile(fileName, contents, successCallback) {
         successCallback()
     }
 }
+*/
 
-function onSaveFileClick() {
+async function onSaveFileClick() {
+    /*
     saveFile(chosenFileName, editedContents, () => {
         chosenFileContents = editedContents
         contentsSaved = editedContents
     })
+    */
+    sha256Saved = await writeToJSONStore(editedContents)
+    chosenFileContents = editedContents
+    contentsSaved = editedContents
 }
 
 function setMode(mode) {
@@ -65,6 +121,7 @@ function setMode(mode) {
 
 function viewFileContents() {
     return m("div",
+        m("div", "SHA256:", sha256Saved || "N/A"),
         m("div",
             m("button", {
                 onclick: () => setMode("view"), 
