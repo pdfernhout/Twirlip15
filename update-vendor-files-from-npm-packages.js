6e20e76d4dadecd44107e8d31129eb91cfd85d47
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2022-05-22 19:11:24 -0400

    vendor: copy some font awesome fonts

--- update-vendor-files-from-npm-packages.js
+++ update-vendor-files-from-npm-packages.js
@@ -19,4 +19,7 @@ function copyFile(source, dest) {
 // Incomplete list
 copyFile("node_modules/mithril/mithril.js", "client/vendor/mithril.js")
 copyFile("node_modules/font-awesome/css/font-awesome.css", "client/vendor/font-awesome/css/font-awesome.css")
+copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.ttf", "client/vendor/font-awesome/fonts/fontawesome-webfont.ttf")
+copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.woff", "client/vendor/font-awesome/fonts/fontawesome-webfont.woff")
+copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.woff2", "client/vendor/font-awesome/fonts/fontawesome-webfont.woff2")
 copyFile("node_modules/marked/lib/marked.esm.js", "client/vendor/marked.js")
