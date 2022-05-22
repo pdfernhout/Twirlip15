6eec9d0fabdbe4b2b81eb38b60d84ccff419ca6a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2022-05-22 18:28:03 -0400

    package: add font-awesome

--- update-vendor-files-from-npm-packages.js
+++ update-vendor-files-from-npm-packages.js
@@ -1,6 +1,21 @@
 // Copy files from npm packages into the vendor directory
 
 const fs = require("fs")
+var path = require("path")
+
+
+function copyFile(source, dest) {
+    const dir1 = path.dirname(dest)
+    const dir2 = path.dirname(dir1)
+    if (!fs.existsSync(dir2)) {
+        fs.mkdirSync(dir2)
+    }
+    if (!fs.existsSync(dir1)) {
+        fs.mkdirSync(dir1)
+    }
+    fs.copyFileSync(source, dest)
+}
 
 // Incomplete list
-fs.copyFileSync("node_modules/mithril/mithril.js", "client/vendor/mithril.js")
+copyFile("node_modules/mithril/mithril.js", "client/vendor/mithril.js")
+copyFile("node_modules/font-awesome/css/font-awesome.css", "client/vendor/font-awesome/css/font-awesome.css")
