2f3f062503075d81785f201b93f45c8ac001f537
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2022-05-22 19:28:55 -0400

    vendor: copy more fount-awesome fonts

--- update-vendor-files-from-npm-packages.js
+++ update-vendor-files-from-npm-packages.js
@@ -18,8 +18,14 @@ function copyFile(source, dest) {
 
 // Incomplete list
 copyFile("node_modules/mithril/mithril.js", "client/vendor/mithril.js")
+
+
 copyFile("node_modules/font-awesome/css/font-awesome.css", "client/vendor/font-awesome/css/font-awesome.css")
+copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.eot", "client/vendor/font-awesome/fonts/fontawesome-webfont.eot")
+copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.svg", "client/vendor/font-awesome/fonts/fontawesome-webfont.svg")
 copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.ttf", "client/vendor/font-awesome/fonts/fontawesome-webfont.ttf")
 copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.woff", "client/vendor/font-awesome/fonts/fontawesome-webfont.woff")
 copyFile("node_modules/font-awesome/fonts/fontawesome-webfont.woff2", "client/vendor/font-awesome/fonts/fontawesome-webfont.woff2")
+copyFile("node_modules/font-awesome/fonts/FontAwesome.otf", "client/vendor/font-awesome/fonts/FontAwesome.otf")
+
 copyFile("node_modules/marked/lib/marked.esm.js", "client/vendor/marked.js")
