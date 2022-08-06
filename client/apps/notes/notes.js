d29d25ee37aa8852f5ba823e9ca268418bedcf7a
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2022-08-06 18:22:25 -0400

    notes: improve stub to have concept list

--- client/apps/notes/notes.js
+++ client/apps/notes/notes.js
@@ -6,19 +6,30 @@
 /* global m */
 import "../../vendor/mithril.js"
 
-// import { Twirlip15ServerAPI } from "../../common/twirlip15-api.js"
+import { Twirlip15ServerAPI } from "../../common/twirlip15-api.js"
 // import { expander } from "../../common/menu.js"
 
+import { ObjectStore } from "../../common/ObjectStore.js"
+
+const twirlipServer = new Twirlip15ServerAPI(error => console.log(error))
+
 let directoryPath
+let o
 
 function loadDirectory() {
     console.log("directoryPath", directoryPath)
+    o = ObjectStore(() => m.redraw(), twirlipServer, directoryPath)
 }
+
+let newConcept = ""
 
 const Notes = {
     view: () => {
-        return m("div.flex.flex-row.h-100.w-100",
-            m("div", "Hello world: ", directoryPath)
+        return m("div.h-100.w-100",
+            (o("concepts", "concept") || []).map(concept => m("div", m("b", concept))),
+            m("br"),
+            m("input", { value: newConcept, oninput: event => newConcept = event.target.value}),
+            m("button", { onclick: () => { o("concepts", "concept", newConcept, "insert"); newConcept = "" }}, "Add concept")
         )
     }
 }
